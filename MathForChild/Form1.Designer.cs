d7811fda37078ceffed37d02e5d537af070489ae
Евгений <evgeniibelov1401@gmail.com>
2025-04-02 21:05:04 +0300

    задал основную логику

--- MathForChild/Form1.Designer.cs
+++ MathForChild/Form1.Designer.cs
@@ -30,22 +30,22 @@
         {
             groupBoxOptions = new GroupBox();
             groupBoxTask = new GroupBox();
+            labelCheckResult = new Label();
+            labelHint = new Label();
+            buttonStart = new Button();
+            buttonCalculate = new Button();
+            labelCharOperation = new Label();
+            labelCharEqual = new Label();
             textBoxResult = new TextBox();
             textBoxNum2 = new TextBox();
             textBoxNum1 = new TextBox();
-            buttonStart = new Button();
-            labelCharEqual = new Label();
-            labelCharOperation = new Label();
-            buttonCalculate = new Button();
-            labelHint = new Label();
-            labelCheckResult = new Label();
             groupBoxTask.SuspendLayout();
             SuspendLayout();
             // 
             // groupBoxOptions
             // 
             groupBoxOptions.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            groupBoxOptions.Location = new Point(64, 32);
+            groupBoxOptions.Location = new Point(24, 32);
             groupBoxOptions.Name = "groupBoxOptions";
             groupBoxOptions.Size = new Size(1384, 125);
             groupBoxOptions.TabIndex = 0;
@@ -57,24 +57,88 @@
             groupBoxTask.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             groupBoxTask.Controls.Add(labelCheckResult);
             groupBoxTask.Controls.Add(labelHint);
+            groupBoxTask.Controls.Add(buttonStart);
             groupBoxTask.Controls.Add(buttonCalculate);
             groupBoxTask.Controls.Add(labelCharOperation);
             groupBoxTask.Controls.Add(labelCharEqual);
             groupBoxTask.Controls.Add(textBoxResult);
             groupBoxTask.Controls.Add(textBoxNum2);
             groupBoxTask.Controls.Add(textBoxNum1);
-            groupBoxTask.Controls.Add(buttonStart);
-            groupBoxTask.Location = new Point(64, 168);
+            groupBoxTask.Location = new Point(32, 168);
             groupBoxTask.Name = "groupBoxTask";
             groupBoxTask.Size = new Size(1016, 184);
             groupBoxTask.TabIndex = 1;
             groupBoxTask.TabStop = false;
             groupBoxTask.Text = "Задание";
             // 
+            // labelCheckResult
+            // 
+            labelCheckResult.AutoSize = true;
+            labelCheckResult.Location = new Point(760, 40);
+            labelCheckResult.Name = "labelCheckResult";
+            labelCheckResult.Size = new Size(50, 20);
+            labelCheckResult.TabIndex = 5;
+            labelCheckResult.Text = "label1";
+            labelCheckResult.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelHint
+            // 
+            labelHint.AutoSize = true;
+            labelHint.Location = new Point(288, 40);
+            labelHint.Name = "labelHint";
+            labelHint.Size = new Size(50, 20);
+            labelHint.TabIndex = 2;
+            labelHint.Text = "label1";
+            labelHint.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            buttonStart.Location = new Point(16, 32);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(232, 48);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "Старт/Заново";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonCalculate
+            // 
+            buttonCalculate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            buttonCalculate.Location = new Point(736, 120);
+            buttonCalculate.Name = "buttonCalculate";
+            buttonCalculate.Size = new Size(232, 48);
+            buttonCalculate.TabIndex = 4;
+            buttonCalculate.Text = "Проверить результат";
+            buttonCalculate.UseVisualStyleBackColor = true;
+            buttonCalculate.Click += buttonCalculate_Click;
+            // 
+            // labelCharOperation
+            // 
+            labelCharOperation.AutoSize = true;
+            labelCharOperation.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelCharOperation.Location = new Point(480, 72);
+            labelCharOperation.Name = "labelCharOperation";
+            labelCharOperation.Size = new Size(30, 31);
+            labelCharOperation.TabIndex = 3;
+            labelCharOperation.Text = "+";
+            labelCharOperation.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // labelCharEqual
+            // 
+            labelCharEqual.AutoSize = true;
+            labelCharEqual.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            labelCharEqual.Location = new Point(720, 72);
+            labelCharEqual.Name = "labelCharEqual";
+            labelCharEqual.Size = new Size(30, 31);
+            labelCharEqual.TabIndex = 2;
+            labelCharEqual.Text = "=";
+            labelCharEqual.TextAlign = ContentAlignment.MiddleCenter;
+            // 
             // textBoxResult
             // 
             textBoxResult.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            textBoxResult.Location = new Point(704, 72);
+            textBoxResult.Location = new Point(760, 72);
             textBoxResult.Name = "textBoxResult";
             textBoxResult.Size = new Size(192, 39);
             textBoxResult.TabIndex = 3;
@@ -83,7 +147,7 @@
             // textBoxNum2
             // 
             textBoxNum2.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            textBoxNum2.Location = new Point(464, 72);
+            textBoxNum2.Location = new Point(520, 72);
             textBoxNum2.Name = "textBoxNum2";
             textBoxNum2.Size = new Size(192, 39);
             textBoxNum2.TabIndex = 2;
@@ -92,73 +156,11 @@
             // textBoxNum1
             // 
             textBoxNum1.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            textBoxNum1.Location = new Point(224, 72);
+            textBoxNum1.Location = new Point(280, 72);
             textBoxNum1.Name = "textBoxNum1";
             textBoxNum1.Size = new Size(192, 39);
             textBoxNum1.TabIndex = 1;
             textBoxNum1.TextAlign = HorizontalAlignment.Center;
-            // 
-            // buttonStart
-            // 
-            buttonStart.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            buttonStart.Location = new Point(24, 32);
-            buttonStart.Name = "buttonStart";
-            buttonStart.Size = new Size(152, 48);
-            buttonStart.TabIndex = 0;
-            buttonStart.Text = "Старт/Заново";
-            buttonStart.UseVisualStyleBackColor = true;
-            // 
-            // labelCharEqual
-            // 
-            labelCharEqual.AutoSize = true;
-            labelCharEqual.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            labelCharEqual.Location = new Point(664, 72);
-            labelCharEqual.Name = "labelCharEqual";
-            labelCharEqual.Size = new Size(30, 31);
-            labelCharEqual.TabIndex = 2;
-            labelCharEqual.Text = "=";
-            labelCharEqual.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // labelCharOperation
-            // 
-            labelCharOperation.AutoSize = true;
-            labelCharOperation.Font = new Font("Segoe UI Black", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            labelCharOperation.Location = new Point(424, 72);
-            labelCharOperation.Name = "labelCharOperation";
-            labelCharOperation.Size = new Size(30, 31);
-            labelCharOperation.TabIndex = 3;
-            labelCharOperation.Text = "+";
-            labelCharOperation.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // buttonCalculate
-            // 
-            buttonCalculate.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            buttonCalculate.Location = new Point(680, 120);
-            buttonCalculate.Name = "buttonCalculate";
-            buttonCalculate.Size = new Size(232, 48);
-            buttonCalculate.TabIndex = 4;
-            buttonCalculate.Text = "Проверить результат";
-            buttonCalculate.UseVisualStyleBackColor = true;
-            // 
-            // labelHint
-            // 
-            labelHint.AutoSize = true;
-            labelHint.Location = new Point(232, 40);
-            labelHint.Name = "labelHint";
-            labelHint.Size = new Size(50, 20);
-            labelHint.TabIndex = 2;
-            labelHint.Text = "label1";
-            labelHint.TextAlign = ContentAlignment.MiddleCenter;
-            // 
-            // labelCheckResult
-            // 
-            labelCheckResult.AutoSize = true;
-            labelCheckResult.Location = new Point(704, 40);
-            labelCheckResult.Name = "labelCheckResult";
-            labelCheckResult.Size = new Size(50, 20);
-            labelCheckResult.TabIndex = 5;
-            labelCheckResult.Text = "label1";
-            labelCheckResult.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // FormMain
             // 
--- MathForChild/Form1.cs
+++ MathForChild/Form1.cs
@@ -2,9 +2,88 @@ namespace MathForChild
 {
     public partial class FormMain : Form
     {
+        private uint num1;
+        private uint num2;
+        private uint MyResult;
         public FormMain()
         {
             InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Execute();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            if (InputData())
+            {
+                textBoxResult.BackColor = Color.Green;
+                labelCheckResult.Visible=true;
+                labelCheckResult.Text = "œ–¿¬»À‹ÕŒ!!!";
+            }
+            else
+            {
+                textBoxResult.BackColor = Color.Red;
+                labelCheckResult.Visible = true;
+                labelCheckResult.Text = "Õ≈œ–¿¬»À‹ÕŒ!!!";
+            }
+        }
+        private uint Sum(uint num1, uint num2)
+        {
+            return num1 + num2;
+        }
+        private uint Minus(uint num1, uint num2)
+        {
+            return num1 + num2;
+        }
+        private bool InputData()
+        {
+            try
+            {
+                if (uint.TryParse(textBoxResult.Text, out uint res))
+                {
+                    this.MyResult = res;
+                    if (MyResult == Sum(num1, num2))return true;
+                    else return false;
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("¬‚Â‰ËÚÂ ÍÓÂÍÚÌ˚Â ‰‡ÌÌ˚Â...");
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+        private void Execute()
+        {
+            Clear();
+            Random rnd = new Random();
+
+            num1 = (uint)rnd.Next(0, 10);
+            num2 = (uint)rnd.Next(0, 10);
+
+            textBoxNum1.Text=num1.ToString();
+            textBoxNum2.Text=num2.ToString();
+
+        }
+        private void Clear()
+        {
+            textBoxNum1.Text=string.Empty;
+            textBoxNum2.Text = string.Empty;
+            textBoxResult.Text = string.Empty;
+            textBoxResult.BackColor = Color.White;
+            labelCheckResult.Visible = false;
+            labelCheckResult.Text = string.Empty;
         }
     }
 }
